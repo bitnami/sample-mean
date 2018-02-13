91da2a45d4c0416ce6a0bfcbab52e6bf7af363e6
Andrés Bono <andresbonojimenez@gmail.com>
2018-02-13 11:47:03 +0100

    Add user validation: script executed by root

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # Load deployment environment
 . /root/.deployment.env
 
